bf675c37665269c3780087dc7580086de449b77e
Adam Conn <adamc@uniform.dev>
2022-06-15 16:59:39 -0700

    Added ability to control where the manifest is read from.

--- worker/src/main.ts
+++ worker/src/main.ts
@@ -1,21 +1,60 @@
 import { createResponse } from 'create-response';
 import { createAkamaiEdgeContext, createAkamaiProxyEdgeHandler } from '@uniformdev/context-edge-akamai';
+import { httpRequest } from 'http-request';
 import { ManifestV2 } from '@uniformdev/context';
-/**
- * The Uniform manifest is available as a local resource.
- * This means if the manifest changes, you must rebuild
- * and deploy the EdgeWorker. 
- * 
- * You might want to deploy the manifest to the origin and 
- * have the EdgeWorker read the manifest from there. This 
- * will allow you to make deploy changes to the manifest 
- * without having to rebuild and deploy the EdgeWorker.
- */
 import manifestJson from './manifest/current.json';
-const manifest: ManifestV2 = <ManifestV2>manifestJson;
+
+/**
+ * You must specify a value for the variable "source".
+ * This value determines how the worker resolves the
+ * Uniform manifest. The following values can be used:
+ * 
+ * OPTION 1: bundle (DEFAULT)
+ * You make the manifest available as a local resource by
+ * downloading it during the build process. Doing this
+ * means that you must rebuild and deploy the EdgeWorker
+ * after any change to the manifest.
+ *
+ * OPTION 2: request
+ * You can deploy the manifest to the origin and have the
+ * EdgeWorker read the manifest from there. This allows
+ * you to make deploy changes to the manifest without 
+ * having to rebuild and deploy the EdgeWorker. 
+ * 
+ * If you use this option you must also do the following:
+ * 1. Include the manifest as a static file (like an image)
+ *    when you deploy your application. For example, if you
+ *    are using Next.js, you would add the manifest file to
+ *    the folder "public".
+ * 2. In the worker, set the variable "url" to the URL.
+ */
+let source = "request";
+const url = "/contextManifest.json";
+ 
+async function getManifest(): Promise<ManifestV2> {
+  if (source === "request") {
+    const response = await httpRequest(url);
+    if (!response.ok) {
+      throw new Error(`Failed to get context manifest from ${url}`);
+    }
+    const manifest = await response.json();
+    return manifest;
+  }
+  if (source === "bundle") {
+    const manifest: ManifestV2 = <ManifestV2>manifestJson;
+    return manifest;
+  }
+  throw new Error(`Unsupported manifest source value ${source}`);
+}
 
 export async function responseProvider(request: EW.ResponseProviderRequest & EW.ReadsVariables) {
   try {
+    
+    /**
+     * How the manifest is read based on environment variables.
+     */
+    const manifest = await getManifest();
+
     /**
      * Since personalization instructions are going 
      * to run in the EdgeWorker, a context object is
